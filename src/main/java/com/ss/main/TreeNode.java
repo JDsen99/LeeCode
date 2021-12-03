006ae02d6f7257c948209061fba80f3848ef1d1a
sensen <64098056@qq.com>
2021-12-03 10:11:40 +0800

    更新 二叉树

---
+++ src/main/java/com/ss/main/TreeNode.java
@@ -0,0 +1,14 @@
+package com.ss.main;
+
+/**
+ * 二叉树
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
