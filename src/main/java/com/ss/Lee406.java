92f758cc21b07f5cfbecbbe3917c10088daafbad
sensen <64098056@qq.com>
2021-12-03 17:59:18 +0800

    更新 添加406

---
+++ src/main/java/com/ss/Lee406.java
@@ -0,0 +1,35 @@
+package com.ss;
+
+import jdk.internal.instrumentation.TypeMappings;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Lee406 {
+
+    public static void main(String[] args) {
+        int[][] p = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+
+        int[][] ints = reconstructQueue(p);
+//        Arrays.sort(p, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+        for (int[] anInt : ints) {
+
+            for (int i : anInt) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int[] i : people) {
+            list.add(i[1], i);
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+}
+
