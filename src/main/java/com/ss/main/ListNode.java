67e1d536c9a75e5e030d33209e0727955a69e43a
sensen <64098056@qq.com>
2021-12-25 16:53:08 +0800

    更新 数据结构 ListNode

---
+++ src/main/java/com/ss/main/ListNode.java
@@ -0,0 +1,23 @@
+package com.ss.main;
+
+/**
+ * @author Senn
+ * @create 2021/12/25 16:14
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
